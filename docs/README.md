760af13b16e440e3080e4403abde7ea38ad7d69e
SuperWhyNot <benjack538@gmail.com>
2026-01-23 03:59:49 +0800

    Rename to Jack, change greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Jack User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Jack.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Jack {
+    private static final String line = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println(line);
+        System.out.println("Hello I'm Jack");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
