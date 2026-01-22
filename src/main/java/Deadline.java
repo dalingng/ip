8181266e0a39765399c33b301c3b1926fa70a15f
SuperWhyNot <benjack538@gmail.com>
2026-01-23 07:53:27 +0800

    added different task types

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,16 @@
+public class Deadline extends Task {
+
+    protected String due;
+
+    public Deadline(String description, String due) {
+        super(description);
+        this.due = due;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + due + ")";
+    }
+}
